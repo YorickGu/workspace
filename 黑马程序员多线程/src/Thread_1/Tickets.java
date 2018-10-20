1fdc8c103cf50f4c9b97474d680d13713bfd4db0
YorickGu <34763540+YorickGu@users.noreply.github.com>
2018-10-20 10:57:25 +0800

    10月20号黑马程序教学

--- 黑马程序员多线程/src/Thread_1/Tickets.java
+++ 黑马程序员多线程/src/Thread_1/Tickets.java
@@ -1,4 +1,39 @@
 package Thread_1;
 
 public class Tickets {
+    public static void main(String[] args) {
+
+    }
+}
+class Company extends Thread{
+
+}
+
+/*
+* 单例饿汉式设计模式
+* */
+class single{
+    private static final single instance = new single();
+    private single(){}
+    public static single getinstance(){
+        return instance;
+    }
+}
+
+/**
+ * 单例懒汉式设计模式
+ * */
+class singleton{
+    private static singleton instance = null;
+    private singleton(){}
+    public static singleton getInstance(){
+        if (instance ==null) {
+            synchronized (singleton.class) {
+                if (instance == null) {
+                    instance = new singleton();
+                }
+            }
+        }
+        return instance;
+    }
 }
